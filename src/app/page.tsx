baa96eff9e1c6301144427973e89dbaaa3d657ad
katboz <136274505+katboz@users.noreply.github.com>
2025-07-19 22:55:12 +0000

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-import Image from "next/image";
+import { type Metadata } from "next";
+import { notFound } from "next/navigation";
+import { asImageSrc } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
+import { createClient } from "@/prismicio";
+//import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
   return (
-      <main className="">
-       <h1 className="text-4xl font-bold">Hello world!!</h1>
-      </main>
-    
-  );
+  <div>
+    <h1>Homepage</h1>
+  </div>
+);
+ // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [{ url: asImageSrc(page.data.meta_image) ?? "" }],
+    },
+  };
 }
