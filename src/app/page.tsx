ce2b80b1b96cc1157b822c70387a3ccab6dc0b30
katboz <136274505+katboz@users.noreply.github.com>
2025-07-19 21:59:55 +0000

    working header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="">
-       <h1 className="text-6xl font-display font-bold">Hello world</h1>
+       <h1 className="text-4xl font-bold">Hello world!!</h1>
       </main>
     
   );
